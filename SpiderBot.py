a63f06fc68144f6d0b71d9a7ac2a74f2c87845ec
t-revor <48257612+t-revor@users.noreply.github.com>
2023-10-13 15:58:53 +0200

    fixed hello event
    added it to a series of events possible upon reading a certain message (by the bot)

--- SpiderBot.py
+++ SpiderBot.py
@@ -27,15 +27,11 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if bot.user.id != message.author.id:
-        if 'hello' in message.content.lower():
-            await message.channel.send(f'Hello{random.choice(["!","!!","!!!"])}')
-    await bot.process_commands(message)
-
-@bot.event
-async def on_message(message):
-    if bot.user.id != message.author.id:
         if ':weh:' in message.content.lower():
             await message.channel.send('<:weh:630081507796582410>')
+            
+        elif 'hello' in message.content.lower():
+            await message.channel.send(f'Hello{random.choice(["!","!!","!!!"])}')
     await bot.process_commands(message)
 
 @bot.command()

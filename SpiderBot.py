355f2b1e5ef26b3ced09dcb564e9284765fb1020
t-revor <48257612+t-revor@users.noreply.github.com>
2023-10-18 17:50:07 +0200

    more fixes

--- SpiderBot.py
+++ SpiderBot.py
@@ -37,10 +37,11 @@ async def on_message(message):
     await bot.process_commands(message)
 
 @bot.event
-async def on_reaction_add(reaction):
-        if reaction.message.author:
-            if reaction.emoji == '<:pog:571499681637466113>':
-                await bot.add_reaction('<:pog:571499681637466113>')
+async def on_reaction_add(reaction, message):
+    if bot.user.id != message.author.id:
+        return
+    if reaction.emoji == '<:pog:571499681637466113>':
+        await bot.add_reaction('<:pog:571499681637466113>')
 
 
 @bot.command()

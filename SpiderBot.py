495931db6de542858b3641d8b9edee43b9541ded
t-revor <48257612+t-revor@users.noreply.github.com>
2024-01-25 10:53:52 +0100

    adding 8ball command

--- SpiderBot.py
+++ SpiderBot.py
@@ -90,6 +90,16 @@ async def coinflip(ctx):
     await ctx.send(random.choice(['headaS','tails']))
 
 @bot.command()
+async def ball(ctx, question: str):
+    """Magic 8 Ball."""
+    ball_answers = [':green_circle: It is certain',':green_circle: It is decidedly so',':green_circle: Without a doubt',':green_circle: Yes definitely',
+                    ':green_circle: You may rely on it',':green_circle: As I see it, yes',':green_circle: Most likely',':green_circle: Outlook good', ':green_circle: Yes',
+                    ':green_circle: Signs point to yes',':yellow_circle: Reply hazy, try again',':yellow_circle: Ask again later',':yellow_circle: Better not tell you now',
+                    ':yellow_circle: Cannot predict now',':yellow_circle: Concentrate and ask again',':red_circle: Do not count on it', ':red_circle: My reply is no',
+                    ':red_circle: My sources say no',':red_circle: Outlook not so good',':red_circle: Very doubtful']
+    await ctx.send(question + ':', random.choice(ball_answers))
+
+@bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
     if times > 10:

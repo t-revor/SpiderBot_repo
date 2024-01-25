0ab1291fc172fb66567626f92ff7bf90f5b63b57
t-revor <48257612+t-revor@users.noreply.github.com>
2024-01-25 11:02:26 +0100

    tuple

--- SpiderBot.py
+++ SpiderBot.py
@@ -97,7 +97,7 @@ async def ball(ctx, question: str):
                     ':green_circle: Signs point to yes',':yellow_circle: Reply hazy, try again',':yellow_circle: Ask again later',':yellow_circle: Better not tell you now',
                     ':yellow_circle: Cannot predict now',':yellow_circle: Concentrate and ask again',':red_circle: Do not count on it', ':red_circle: My reply is no',
                     ':red_circle: My sources say no',':red_circle: Outlook not so good',':red_circle: Very doubtful']
-    reply = question + ':', random.choice(ball_answers)
+    reply = (question + ':', random.choice(ball_answers))
     await ctx.send(reply)
 
 @bot.command()
